a118eb71c069f895a7995a6ffa35fe9dda359a35
StanislawKopec <91846304+StanislawKopec@users.noreply.github.com>
2024-08-13 18:48:15 +0200

    fix faulty image import

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import React from "react";
-import heroImg from "../assets/jennie.png";
 import food1 from "../assets/food1.jpg";
 import food2 from "../assets/food2.jpg";
 import food3 from "../assets/food3.jpg";
